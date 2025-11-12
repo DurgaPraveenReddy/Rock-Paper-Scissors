f22fb34352f5ee13854642e41e1979f02c5d35c1
Durga Praveen Reddy <durgapraveennaidu@gmail.com>
2025-11-12 06:38:50 +0530

    Iteration-2
    created getComputerChoice() function that returns rock/paper/scissors.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello!");
+// computer choice function
+let getComputerChoice = () => {
+    let val = Math.random()
+    let choice
+    if(val < 0.4){
+        choice = "Rock"
+    }else if(val > 0.4 && val < 0.7){
+        choice = "Paper"
+    }else{
+        choice = "Scissors"
+    }
+    return choice
+}
+console.log(getComputerChoice())
