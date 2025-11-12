89c1c8ab716eff944bb79d967168e9ed7da2e34b
Durga Praveen Reddy <durgapraveennaidu@gmail.com>
2025-11-12 07:42:35 +0530

    Iteration-3
    created getHumanChoice() function which returns the choice picked by the user through prompt.

--- script.js
+++ script.js
@@ -11,4 +11,22 @@ let getComputerChoice = () => {
     }
     return choice
 }
-console.log(getComputerChoice())
+
+//human choice function
+let getHumanChoice = function() {
+    let picked
+    let choice = parseInt(prompt("Enter your choice: 1.Rock 2.Paper 3.Scissors"))
+    if(choice === 1){
+        picked = "Rock"
+    }else if(choice === 2){
+        picked = "Paper"
+    }else if(choice === 3){
+        picked = "Scissors"
+    }
+    else{
+        console.log("Invalid choice. Returning null.")
+        picked = null
+    }
+    return picked
+}
+console.log(getHumanChoice())
